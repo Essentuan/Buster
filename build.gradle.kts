c07860084089b83d3cace4b0327141aa8df7234e
Essentuan <51426319+Essentuan@users.noreply.github.com>
2024-12-22 15:50:18 -0600

    Ensure mavenLocal publication is same as jitpack.

--- build.gradle.kts
+++ build.gradle.kts
@@ -69,6 +69,9 @@ tasks {
 publishing {
     publications {
         create<MavenPublication>("maven") {
+            group = "com.github.essentuan"
+            version = "v${project.version}"
+
             artifactId = "buster"
 
             from(components["java"])
