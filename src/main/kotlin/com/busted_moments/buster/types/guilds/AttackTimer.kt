6ffc25900d37253c4c99a5a1a66604ab79823f70
spinningrat69 <125729313+spinningrat69@users.noreply.github.com>
2025-11-08 02:39:44 +0200

    IntelliJ bad

--- src/main/kotlin/com/busted_moments/buster/types/guilds/AttackTimer.kt
+++ src/main/kotlin/com/busted_moments/buster/types/guilds/AttackTimer.kt
@@ -2,12 +2,10 @@ package com.busted_moments.buster.types.guilds
 
 import com.busted_moments.buster.Buster
 import com.busted_moments.buster.api.Territory
-import kotlin.math.abs
 import net.essentuan.esl.time.duration.Duration
+import net.essentuan.esl.time.duration.seconds
 import net.essentuan.esl.time.extensions.timeUntil
 import java.util.Date
-
-private const val TEN_SECONDS = 10000L
 
 data class AttackTimer(
     val territory: String,
@@ -15,8 +13,6 @@ data class AttackTimer(
     val defense: Territory.Rating,
     val trusted: Boolean
 ) : Buster.Type {
-    val id: String = java.lang.Long.toHexString(endsAt.time / TEN_SECONDS)
-
     val remaining: Duration
         get() = endsAt.timeUntil()
 
@@ -28,16 +24,22 @@ data class AttackTimer(
         if (other !is AttackTimer) return false
 
         if (territory != other.territory) return false
-        if (abs(endsAt.time - other.endsAt.time) >= TEN_SECONDS) return false
+
+        val diff = (remaining - other.remaining).abs()
+        if (diff < MARGIN_OF_ERROR)
+            return false
 
         return true
     }
 
-    override fun hashCode(): Int {
-        return territory.hashCode()
-    }
+    override fun hashCode(): Int =
+        territory.hashCode()
 
     override fun toString(): String {
-        return "AttackTimer(id='$id', territory='$territory', endsAt=$endsAt, defense=$defense, trusted=$trusted)"
+        return "AttackTimer(territory='$territory', endsAt=$endsAt, defense=$defense, trusted=$trusted)"
+    }
+
+    companion object {
+        val MARGIN_OF_ERROR = 20.seconds
     }
 }
